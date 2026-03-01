350d3906438b9a88e12a2f79a1aef020a2540303
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2026-03-01 07:48:48 -0300

    criando 1º rota tipo post

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
-app.get('/usuarios', (req, res) => {
+app.get('/usuarios/:abacate', (req, res) => {
+    console.log(req)
+
    res.send("olá")
+})
+
+app.post('/usuarios', (req, res) => {
+   console.log(req)
+
+   res.send("ola")
 })
 
 app.listen(3000)
